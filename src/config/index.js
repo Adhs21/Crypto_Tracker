4c8ecc15810e18671af1e1b0ecb9d8b4bd9dfdc8
Atharv Sasturkar <sasturkaratharv@gmail.com>
2025-08-07 13:36:20 +0530

    Add index.html for GitHub Pages

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "../App";
 import "react-alice-carousel/lib/alice-carousel.css";
-import CryptoContext from "./CryptoContext";
+import CryptoContext from "../CryptoContext";
 
 ReactDOM.render(
   <React.StrictMode>
